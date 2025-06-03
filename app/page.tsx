c8317b6c5690d1ca05f104ed50217d57281248a3
Jordiestrada3 <71834806+Jordiestrada3@users.noreply.github.com>
2025-06-03 19:11:30 +0200

    ADD, first section basics added

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <header>
+      {/* <header>
         <h1>Jordi Estrada Ylla</h1>
         <ul>
           <li></li>
@@ -12,18 +12,22 @@ export default function Home() {
           <li></li>
         </ul>
         
-      </header>
+      </header> */}
       <main>
-        
+        <section style={{backgroundColor:'lightgrey', width:'100vw', height:'100vh', display: 'flex', flexDirection:'column', justifyContent: 'center', alignItems: 'center' }}>
+          <h2>Hi, my name is</h2>
+          <h1>Jordi</h1>
+          <h2>I'm a full stack developer</h2>
+        </section>
       </main>
-      <footer>
+      {/* <footer>
         <ul style={{ display: 'flex', flexDirection: 'row', padding: 20, gap: 10 }}>
           <li>hola</li>
           <li>adeu</li>
           <li>patata</li>
           <li>ceba</li>
         </ul>
-      </footer>
+      </footer> */}
     </div>
   );
 }
