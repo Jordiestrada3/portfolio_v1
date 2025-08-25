8242127fa563807ce9f2bb99e24bcadda8641787
Jordiestrada3 <71834806+Jordiestrada3@users.noreply.github.com>
2025-08-25 18:51:52 +0200

    ADD, Logos with animation added

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,22 @@
 import { LettersPullUp } from "./animations/letters-pull-up";
-import { TypingEffect } from "./animations/typing-effect";
 import { WordsPullUp } from "./animations/words-pull-up";
+import Image from "next/image";
+
 
 export default function Home() {
+
+  const logos = [
+    "/logos/HTML.png",
+    "/logos/CSS.png",
+    "/logos/JavaScript.png",
+    "/logos/TypeScript.png",
+    "/logos/React.png",
+    "/logos/Next.js.png",
+    "/logos/Node.js.png",
+    "/logos/GitHub.png",
+    "/logos/Agile.png"
+  ]
+
   return (
     <div>
       {/* <header>
@@ -72,28 +86,23 @@ export default function Home() {
             gap: 50,
             alignItems: 'flex-start',
           }}>
-            <div style={{ width: '50%', display: 'flex', flexWrap: 'wrap', gap: 10 }}>
-              <div style={{ width: 100, height: 100, backgroundColor: 'lightgrey', borderRadius: 10 }} />
-              <div style={{ width: 100, height: 100, backgroundColor: 'lightgrey', borderRadius: 10 }} />
-              <div style={{ width: 100, height: 100, backgroundColor: 'lightgrey', borderRadius: 10 }} />
-              <div style={{ width: 100, height: 100, backgroundColor: 'lightgrey', borderRadius: 10 }} />
-              <div style={{ width: 100, height: 100, backgroundColor: 'lightgrey', borderRadius: 10 }} />
-              <div style={{ width: 100, height: 100, backgroundColor: 'lightgrey', borderRadius: 10 }} />
-              <div style={{ width: 100, height: 100, backgroundColor: 'lightgrey', borderRadius: 10 }} />
-              <div style={{ width: 100, height: 100, backgroundColor: 'lightgrey', borderRadius: 10 }} />
-              <div style={{ width: 100, height: 100, backgroundColor: 'lightgrey', borderRadius: 10 }} />
-              <div style={{ width: 100, height: 100, backgroundColor: 'lightgrey', borderRadius: 10 }} />
-              <div style={{ width: 100, height: 100, backgroundColor: 'lightgrey', borderRadius: 10 }} />
-              <div style={{ width: 100, height: 100, backgroundColor: 'lightgrey', borderRadius: 10 }} />
-              <div style={{ width: 100, height: 100, backgroundColor: 'lightgrey', borderRadius: 10 }} />
-              <div style={{ width: 100, height: 100, backgroundColor: 'lightgrey', borderRadius: 10 }} />
-              <div style={{ width: 100, height: 100, backgroundColor: 'lightgrey', borderRadius: 10 }} />
-              <div style={{ width: 100, height: 100, backgroundColor: 'lightgrey', borderRadius: 10 }} />
-              <div style={{ width: 100, height: 100, backgroundColor: 'lightgrey', borderRadius: 10 }} />
-              <div style={{ width: 100, height: 100, backgroundColor: 'lightgrey', borderRadius: 10 }} />
-              <div style={{ width: 100, height: 100, backgroundColor: 'lightgrey', borderRadius: 10 }} />
-              <div style={{ width: 100, height: 100, backgroundColor: 'lightgrey', borderRadius: 10 }} />
-              <div style={{ width: 100, height: 100, backgroundColor: 'lightgrey', borderRadius: 10 }} />
+            <div style={{ width: '50%', display: 'flex', flexWrap: 'wrap', gap: 30 }}>
+              {logos.map((logo, index) => {
+
+                const alt = `Logo ${logo.split('/').pop().split('.')[0]}`
+                const tech = logo.split('/').pop().split('.')[0]
+
+                return <div key={index} className="logo-card">
+                  <img
+                    key={index}
+                    src={logo}
+                    alt={alt}
+                    width={100}
+                    height={100}
+                  />
+                  <p className="logo-text">{tech}</p>
+                </div>
+              })}
             </div>
             <div className="vertical-line"></div>
             <h2 style={{
@@ -105,19 +114,9 @@ export default function Home() {
               <WordsPullUp text="Skill Set" className="skill-set-title"></WordsPullUp>
               {/* Skill Set */}
             </h2>
-
-
           </div>
         </section>
       </main>
-      {/* <footer>
-        <ul style={{ display: 'flex', flexDirection: 'row', padding: 20, gap: 10 }}>
-          <li>hola</li>
-          <li>adeu</li>
-          <li>patata</li>
-          <li>ceba</li>
-        </ul>
-      </footer> */}
     </div >
   );
 }
